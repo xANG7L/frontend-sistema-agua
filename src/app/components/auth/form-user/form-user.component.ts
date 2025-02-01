9976374e812d7ae73f3e675ce8c1634855f64a04
ANGEL <ang7l.manciaa@gmail.com>
2025-02-01 14:17:16 -0600

    filtro implementado de web app

--- src/app/components/auth/form-user/form-user.component.ts
+++ src/app/components/auth/form-user/form-user.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { crearUsuario } from '../../../store/auth/auth.actions';
+import { SharingDataService } from '../../../services/sharing-data.service';
+import { AuthService } from '../../../services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 export interface IUserForm {
   username: string;
@@ -34,16 +38,31 @@ export class FormUserComponent {
   creando: boolean = false;
 
   constructor(
-    private store: Store<{ auth: any }>
+    private _authService: AuthService
   ) {
-    this.store.select('auth').subscribe(state => {
-      this.creando = state.cargando;
-      this.errors = state.errors;
-    })
+
   }
 
   onSubmit(userForm: NgForm): void {
-    this.store.dispatch(crearUsuario({ usuario: this.user}));
+    this.creando = true;
+    this.errors = {};
+    this._authService.postCrearUsuario(this.user).subscribe({
+      next: () => {
+        this.creando = false;
+        Swal.fire('Usuario creado correctamente!!', '', 'success');
+        userForm.resetForm();
+      },
+      error: (err: HttpErrorResponse) => {
+        this.creando = false;
+        if (err.status == 400) {
+          this.errors = err.error;
+        } else {
+          Swal.fire('Ocurrio un error', 'Intente crear el usuario mas tarde', 'error');
+
+        }
+      }
+    })
+    // this.store.dispatch(crearUsuario({ usuario: this.user}));
   }
 
 }
